40cb18dac91f2b297631320dee7965098c347089
jiangkang <jkhhuse@126.com>
2021-12-19 23:04:22 +0800

    增加sql处理程序

--- sql.js
+++ sql.js
@@ -12,17 +12,49 @@ import fs from "fs";
 //   database: 'plan',
 // });
 
-const file = fs.readFileSync("file.txt", "utf8");
+const file = fs.readFileSync("test.txt", "utf8");
 
-const lines = file.split("\r\n");
+const lines = file.split("\n");
 
+const sqlList = [];
+
+// insert into food (name, protein, phe, rule, type) values ("xxx", 3.2, 100.25, "red", "accurate");
 for (let i = 0; i < lines.length; i++) {
-  const 
+  const line = lines[i].split("|");
+  const name = line[0];
+  const protein = line[1];
+  let phe = line[2];
+  let type = "accurate";
+  // 存在 phe 值则为精确值
+  if (phe) {
+    type = "accurate";
+  } else {
+    type = "estimate";
+  }
+  // 如果 phe 不存在，则使用蛋白质 * 50 进行估算
+  if (phe === "-") {
+    if (protein === "-") {
+      phe = "-";
+    } else {
+      phe = protein * 50;
+    }
+  }
+  let rule = "red";
+  if (phe > 50) {
+    rule = "red";
+  } else if ((phe <= 50) & (phe > 20)) {
+    rule = "yellow";
+  } else {
+    rule = "green";
+  }
+  const sql = `(${name}, ${protein}, ${phe}, ${rule}, ${type})`;
+  sqlList.push(sql);
 }
 
-console.log(lines);
-
-// simple query
-// const [rows, fields] = await connection.query('SELECT * FROM density');
-
-// console.log(fields);
+connection.execute(
+  "insert into food (name, protein, phe, rule, type) values ?",
+  [sqlList],
+  (err, rows) => {
+    console.log(rows);
+  }
+);
